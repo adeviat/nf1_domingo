bb85f990bf77569d914992277d6a1a75acf2cd58
Mostapha <mostapha-ach@hotmail.com>
2019-11-11 21:40:55 +0100

    Backend Mostapha

--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import DomingoSignIn from './DomingoSignIn'
-import TestView from "./TestView";
 import DialogEditUser from './DialogEditUser.js'
 import HomePage from'./components/HomePage/HomePage.js'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HomePage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
